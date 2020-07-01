d17ea5d7c14e12db4de3070765f6b72db8023608
the-real-jk <the-real-jk@users.noreply.github.com>
2020-07-01 15:47:38 +0200

    index.css: changed margin login.css: added some new styles login.jsx: created a login form background2.jpg: added possible background image for Login form.     Not implemented yet as it caused me some trouble backgroundLogin.jpg added another possible background image. bushfire.jpg: deleted file

--- src/accounts/login.jsx
+++ src/accounts/login.jsx
@@ -1,13 +1,59 @@
 import React from "react";
 import "./login.css";
+import "../index.css";
 import TextField from "@material-ui/core/TextField";
 import store from "../app/store";
+import { Grid, Typography } from "@material-ui/core";
 
 export default class Login extends React.Component {
   render() {
     return (
       <div>
-        <form id="loginForm"></form>
+        {/* login form */}
+        <Grid
+          container
+          className="formContainer"
+          justify="center"
+          alignItems="center"
+          direction="column"
+          spacing={0}
+        >
+          <Grid item xs={12} spacing={1}>
+            <Typography
+              variant="h2"
+              style={{ color: "white" }}
+              className="center"
+            >
+              Login
+            </Typography>
+            <form>
+              {/* username */}
+              <div>
+                <TextField
+                  required
+                  id="usernameInputLogin"
+                  autoComplete="off"
+                  type="text"
+                  label="Username"
+                  variant="outlined"
+                  fullWidth
+                ></TextField>
+              </div>
+              {/* password */}
+              <div>
+                <TextField
+                  required
+                  id="passwordInputLogin"
+                  autoComplete="off"
+                  type="password"
+                  label="Passwort"
+                  variant="outlined"
+                  fullWidth
+                ></TextField>
+              </div>
+            </form>
+          </Grid>
+        </Grid>
       </div>
     );
   }
