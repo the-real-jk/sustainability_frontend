b85838f6486c0daee51dfef5cd9555bdcd02ea1e
the-real-jk <the-real-jk@users.noreply.github.com>
2020-07-02 08:42:55 +0200

    sustainabilityReducer.jsx: reducer now uses spread operater instead of Object.assign

--- src/app/reducers/sustainabilityReducer.jsx
+++ src/app/reducers/sustainabilityReducer.jsx
@@ -10,15 +10,9 @@ function SustainabilityReducer(state = initialState, action) {
 
   switch (action.type) {
     case "LOGGED_IN":
-      return Object.assign({}, state, {
-        loggedIn: true,
-        loggedOut: false,
-      });
+      return { ...state, loggedIn: true, loggedOut: false };
     case "LOGGED_OUT":
-      return Object.assign({}, state, {
-        loggedIn: false,
-        loggedOut: true,
-      });
+      return { ...state, loggedIn: false, loggedOut: true };
     default:
       return state;
   }
